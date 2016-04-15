f1564a068ca3e934bd96d00275e5e6ca050c7bf9
Rooxo <rooxoxoxoxo@gmail.com>
2016-04-15 19:53:44 +0200

    simpified renamePattern

--- renamePattern.sh
+++ renamePattern.sh
@@ -1,43 +1,22 @@
 USAGE="Usage: renamePattern.sh [OPTIONS] SEARCHSTR REPLACESTR"
 MOREINFO="try 'renamePattern.sh -h' for more information"
-if [ "$#" = "0" ] || [ "$#" = "1" ]
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]
 then
-	echo "Not enough arguments"
 	echo $USAGE
-	echo $MOREINFO
+	echo "Search for occurences of SEARCHSTR in filenames in current folder and subfolders and replace it with REPLACESTR"
+	echo "Example: renamePattern.sh ' ' '_'"
+	echo ""
+	echo "Options:"
+	echo "-h, --help	Print this help and exit"
 	exit
+elif [ "$#" = "1" ] || [ "$#" = "0" ]
+then
+	echo "not enough arguments"
+	echo $USAGE
+	echo "see 'renamePattern.sh -h' for more information"
 fi
 
-while [ "$#" -ne "2" ]
-do
-	case $1 in
-		"-h"| "--help")
-		echo $USAGE
-		echo "Search for occurences of SEARCHSTR in filenames in current folder and subfolders and replace it with REPLACESTR"
-		echo "Example: renamePattern.sh ' ' '_'"
-		echo ""
-		echo "Options:"
-		echo "-h, --help	Print this help and exit"
-		echo "-D, --directory	The directory in which to search for files"
-		echo "-d, --depth	max depth to search for files"
-		exit
-		;;
-		"-D"| "--directory")
-		SEARCHDIR=$2
-		shift
-		;;
-		"-d"|"--depth")
-		SEARCHDEPTH=$2
-		shift
-		;;
-		*)
-		echo "renamePattern: unknown argument $1"
-		echo $USAGE
-		echo $MOREINFO
-		exit
-	esac
-	shift
-done
 SEARCHSTR=$1
 shift
 REPLACESTR=$1
