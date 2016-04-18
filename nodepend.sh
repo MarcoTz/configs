084f92c2d03f3988295f157df7b221da2367e818
Rooxo <rooxoxoxoxo@gmail.com>
2016-04-18 12:21:34 +0200

    updated nodepend

--- nodepend.sh
+++ nodepend.sh
@@ -1,7 +1,13 @@
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]
 then
 	echo "lists all packages no other packages depend on(pacman only)"
+	echo ""	
+	echo "Options:"
+	echo "-o, --orphans removes orphans first(requires root permissions)"	
 	exit
+elif [ "$1" == "-o" ] || [ "$1" == "--orphans" ]
+then
+	pacman -Rns $(pacman -Qtdq)
 fi
 
 ignoregrp="base base-devel"
