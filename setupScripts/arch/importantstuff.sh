85cc475ef37f6ab9c14aa6f052a22310091a1d4b
Rooxo <rooxoxoxoxo@gmail.com>
2016-05-18 19:50:17 +0200

    updated important stuff

--- setupScripts/arch/importantstuff.sh
+++ setupScripts/arch/importantstuff.sh
@@ -1,4 +1,4 @@
-pacman -S vim git chromium wine python
+pacman -S vim git chromium wine base-devel jdk80-openjdk mariadb rfkill
 
 #for syntax checking in syntastic, also because I like to feel bad about my code
 pacman -S python-pylint
