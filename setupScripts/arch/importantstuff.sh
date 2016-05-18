cd723cfeb953c69123c4e88a5f08fdfa0a26bf64
Rooxo <rooxoxoxoxo@gmail.com>
2016-05-18 19:30:42 +0200

    removed syntaxcheckers

--- setupScripts/arch/importantstuff.sh
+++ setupScripts/arch/importantstuff.sh
@@ -1,1 +1,4 @@
-pacman -S vim git chromium
+pacman -S vim git chromium wine python
+
+#for syntax checking in syntastic, also because I like to feel bad about my code
+pacman -S python-pylint
--- setupScripts/arch/syntaxCheckers.sh
+++
@@ -1,2 +0,0 @@
-#pylint python, also pretty awesome to feel bad about your code
-pacman -S python-pylint
