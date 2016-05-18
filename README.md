dccdfdfa0a68638d7714017454c17ec12c5dbed1
Rooxo <rooxoxoxoxo@gmail.com>
2016-05-18 17:50:16 +0200

    updated vim and bashrc and readme

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ other repos I used:
 
 * https://github.com/tpope/vim-pathogen
 * https://github.com/scrooloose/nerdtree
-* https://github.com/chriskempson/base16 (Xresources version, for urxvt)
+* https://github.com/chriskempson/base16 (Xresources and vim version)
--- setupScripts/vimplugins.sh
+++ setupScripts/vimplugins.sh
@@ -6,8 +6,17 @@ cd ~/.vim
 mkdir "autoload" "bundle"
 git clone "https://github.com/tpope/vim-pathogen.git"
 mv "vim-pathogen/autoload/pathogen.vim" "autoload"
+rm "vim-pathogen" -rf
 
 #install nerdtree
 cd bundle
 git clone https://github.com/scrooloose/nerdtree.git
+cd ../
+
+#install base16 colorscheme
+mkdir colors
+cd colors
+git clone git://github.com/chriskempson/base16-vim.git
+cp "base16-vim/colors/*" ./
+rm -rf base16-vim
 cd ../
