af866be7a087f838fcee95baff825733ee799040
Rooxo <rooxoxoxoxo@gmail.com>
2016-05-19 15:53:18 +0200

    updated readme moved some scripts updated installconfigs '

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Some random bash scripts I made for various things
 
+directories:
+
+* configs: files i use to save my settings like bashrc and vimrc
+	(used with symlinks)
+* utils: some bash scripts that do some useful things
+	(folder in PATH)
+* setupscripts: scripts that install various things
+	(including a script that makes the links for my config files)
+
 other repos I used:
 
 * https://github.com/tpope/vim-pathogen
--- setupScripts/installConfs.sh
+++ setupScripts/installConfs.sh
@@ -1,7 +1,26 @@
 $DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 cd ../configs
+
+if [ "$1" == "-f" ] || [ "$1" == "--force" ]
+then 
+	rm ~/.bashrc
+	rm ~/.vimrc
+	rm ~/.Xresources
+	rm ~/.config/flake8
+fi
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]
+then
+	echo "installs configs in config directory of this repo"
+	echo "options:"
+	echo "-h, --help print this help and exit"
+	echo "-f, --force remove the configs first if they exist"
+fi
+
 ln -s bashrc ~/.bashrc
 ln -s vimrc ~/.vimrc
 ln -s Xresources ~/.Xresources
+xrdb ~/.Xresources
 ln -s flake8 ~/.config/flake8
+
