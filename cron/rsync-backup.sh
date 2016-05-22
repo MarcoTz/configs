94cbe2778dc2cd2f0fb60d0514f1c4116f5f6ca9
Rooxo <rooxoxoxoxo@gmail.com>
2016-05-22 11:56:26 +0200

    fixed rsync-backup

--- cron/rsync-backup.sh
+++ cron/rsync-backup.sh
@@ -1,8 +1,8 @@
 BACKUPDIR="/run/media/rooxo/a03d2c3e-258b-462f-a360-1b397db1f607/"
 
-rsync -a --delete ~/Documents $BACKUPDIR/Documents
-rsync -a --delete ~/Music $BACKUPDIR/Music
-rsync -a --delete ~/Apps $BACKUPDIR/Apps
-rsync -a --delete ~/Games $BACKUPDIR/Games
-rsync -a --delete ~/Pictures $BACKUPDIR/Pictures
-rsync -a --delete ~/Videos $BACKUPDIR/Videos
+rsync -a --delete ~/Documents $BACKUPDIR
+rsync -a --delete ~/Music $BACKUPDIR
+rsync -a --delete ~/Apps $BACKUPDIR
+rsync -a --delete ~/Games $BACKUPDIR
+rsync -a --delete ~/Pictures $BACKUPDIR
+rsync -a --delete ~/Videos $BACKUPDIR
