8a10ede26d573ac5ed871beb7a81e7071d8555d4
Rooxo <rooxoxoxoxo@gmail.com>
2016-06-14 00:42:44 +0200

    updated importantstuff

--- setupScripts/arch/importantstuff.sh
+++ setupScripts/arch/importantstuff.sh
@@ -1,5 +1,5 @@
 #installs important packages, requires root
-pacman -S vim git wine base-devel jdk8-openjdk mariadb rfkill vlc python-pip frcon virtualbox
+pacman -S vim git wine base-devel jdk8-openjdk mariadb rfkill python-pip frcon
 
 systemctl start fcron.service
 systemctl enable fcron.service
