6d63b8484b159e95ab0b830b9f1558ef8ff7560d
Rooxo <rooxoxoxoxo@gmail.com>
2016-12-30 19:04:52 +0100

    changed rsync to unison

--- utils/rsync-backup.sh
+++
@@ -1,9 +0,0 @@
-BACKUPDIR="/run/media/rooxo/a03d2c3e-258b-462f-a360-1b397db1f607/"
-
-rsync -a --delete ~/Documents $BACKUPDIR
-rsync -a --delete ~/Music $BACKUPDIR
-rsync -a --delete ~/Apps $BACKUPDIR
-rsync -a --delete ~/Games $BACKUPDIR
-rsync -a --delete ~/Pictures $BACKUPDIR
-rsync -a --delete ~/Videos $BACKUPDIR
-rsync -a --delete ~/Desktop $BACKUPDIR
---
+++ utils/unisonsync.sh
@@ -0,0 +1,9 @@
+BACKUPDIR="/run/media/rooxo/a03d2c3e-258b-462f-a360-1b397db1f607/"
+
+unison ~/Apps $BACKUPDIR/Apps
+unison ~/Documents $BACKUPDIR/Documents
+unison ~/Games $BACKUPDIR/Games
+unison ~/Desktop $BACKUPDIR/Desktop
+unison ~/Music $BACKUPDIR/Music
+unison ~/Pictures $BACKUPDIR/Pictures
+unison ~/Videos $BACKUPDIR/Videos
