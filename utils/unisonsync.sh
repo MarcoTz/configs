d9dc9a1da51ad3e679cdbf7740b141c234d739ec
Rooxo <rooxoxoxoxo@gmail.com>
2016-12-30 19:17:45 +0100

    updated unisonsync

--- utils/unisonsync.sh
+++ utils/unisonsync.sh
@@ -1,9 +1,9 @@
 BACKUPDIR="/run/media/rooxo/a03d2c3e-258b-462f-a360-1b397db1f607/"
 
-unison ~/Apps $BACKUPDIR/Apps
-unison ~/Documents $BACKUPDIR/Documents
-unison ~/Games $BACKUPDIR/Games
-unison ~/Desktop $BACKUPDIR/Desktop
-unison ~/Music $BACKUPDIR/Music
-unison ~/Pictures $BACKUPDIR/Pictures
-unison ~/Videos $BACKUPDIR/Videos
+unison ~/Apps $BACKUPDIR/Apps -auto
+unison ~/Documents $BACKUPDIR/Documents -auto
+unison ~/Games $BACKUPDIR/Games -auto
+unison ~/Desktop $BACKUPDIR/Desktop -auto
+unison ~/Music $BACKUPDIR/Music -auto
+unison ~/Pictures $BACKUPDIR/Pictures -auto
+unison ~/Videos $BACKUPDIR/Videos -auto
