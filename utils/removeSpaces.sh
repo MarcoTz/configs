bda837b59b728fafad5b1a874c7d1891e9aa0e32
Rooxo <rooxoxoxoxo@gmail.com>
2017-01-02 00:54:55 +0100

    initiated branch

--- utils/removeSpaces.sh
+++
@@ -1,7 +0,0 @@
-if [[ "$1" = "-h"  ]] || [[ "$1" = "--help" ]]
-then
-	echo "removes all spaces in files in current folder, replacing them with _"
-	exit
-fi
-
-find . -maxdepth 1 -name "* *" | while IFS= read -r f ; do mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr " " "_")" ; done
