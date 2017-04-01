38f22fd52321c4a5852528506dfea15ab8266f58
rooxo <rooxoxoxoxo@gmail.com>
2017-04-01 10:51:06 +0200

    updated scripts

--- utils/unisonsync.sh
+++ utils/unisonsync.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 BACKUPDIR="/mnt/"
 
-unison ~/Apps $BACKUPDIR/Apps -auto
-unison ~/Documents $BACKUPDIR/Documents -auto
-unison ~/Games $BACKUPDIR/Games -auto
-unison ~/Music $BACKUPDIR/Music -auto
+unison ~/Documents/Studium $BACKUPDIR/Documents/Studium -auto
+unison ~/Documents/ebooks $BACKUPDIR/Documents/ebooks -auto
 unison ~/Pictures $BACKUPDIR/Pictures -auto
-unison ~/Videos $BACKUPDIR/Videos -auto
