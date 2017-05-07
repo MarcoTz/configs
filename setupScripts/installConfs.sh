83931ab48aad6be66278647603f462810578ce07
rooxo <rooxoxoxoxo@gmail.com>
2017-05-07 12:39:02 +0200

    added rofi some other stuff

--- setupScripts/installConfs.sh
+++ setupScripts/installConfs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 cd $DIR
 
 #removes old config files if -f or --force is present
@@ -7,6 +7,7 @@ if [ "$1" == "-f" ] || [ "$1" == "--force" ]
 then 
 	rm ~/.bashrc
 	rm ~/.vimrc
+	rm ~/.Xresources
 	rm ~/.i3/config
 	rm ~/.i3/conky.sh
 	rm ~/.i3/workspace_msg.json
@@ -26,13 +27,14 @@ fi
 
 ln -s $DIR/configs/bashrc ~/.bashrc
 ln -s $DIR/configs/vimrc ~/.vimrc
+ln -s $DIR/configs/Xresources ~/.Xresources
 mkdir ~/.unison
 cp $DIR/configs/unison.prf ~/.unison/default.prf
 mkdir ~/.i3
 ln -s $DIR/configs/i3 ~/.i3/config
 ln -s $DIR/configs/conky.sh ~/.i3/conky.sh
 ln -s $DIR/configs/workspaces/msg.json ~/.i3/workspace_msg.json
-ln -s $DIR/configs/workspaces/rss.json ~/.i3/workpsace_rss.json
+ln -s $DIR/configs/workspaces/rss.json ~/.i3/workspace_rss.json
 mkdir ~/.config
 ln -s $DIR/configs/conkyrc ~/.config/conkyrc
 mkdir ~/.config/termite
