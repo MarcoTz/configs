5fdbdf8c13486eed6c57270c6d60714d30ca0d5b
rooxo <rooxoxoxoxo@gmail.com>
2017-07-20 14:12:10 +0200

    finished rotate script

--- utils/rotate.sh
+++ utils/rotate.sh
@@ -4,16 +4,19 @@
 if [ "$1" == 0 ] 
 then
 	xrandr --output eDP1 --rotate normal
+	xinput --set-prop "ATML1000:00 03EB:8C3C" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
 fi
 
 #rotate left
 if [ "$1" == 1 ]
 then
 	xrandr --output eDP1 --rotate left
+	xinput --set-prop "ATML1000:00 03EB:8C3C" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
 fi
 
 #rotate right
 if [ "$1" == 2 ]
 then 
 	xrandr --output eDP1 --rotate right
+	xinput --set-prop "ATML1000:00 03EB:8C3C" "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
 fi
