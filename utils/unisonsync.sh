b8ed9b1bb359155cdb616209ecbc5ae6b933502a
rooxo <rooxoxoxoxo@gmail.com>
2017-07-27 17:19:01 +0200

    added rss feeds to unisonsync

--- utils/unisonsync.sh
+++ utils/unisonsync.sh
@@ -4,3 +4,4 @@ BACKUPDIR="/mnt/"
 unison ~/Documents/Studium $BACKUPDIR/Documents/Studium -auto
 unison ~/Documents/ebooks $BACKUPDIR/Documents/ebooks -auto
 unison ~/Pictures $BACKUPDIR/Pictures -auto
+unison ~/Documents/feedlist.opml -auto
