a268d63d6094607bafdea7e6b0076fe5fa5a880c
rooxo <rooxoxoxoxo@gmail.com>
2017-07-27 09:12:10 +0200

    updated nodepend

--- utils/nodepend.sh
+++ utils/nodepend.sh
@@ -9,15 +9,24 @@ then
 	echo "Options:"
 	echo "-o, --orphans removes orphans(requires root permissions)"	
 	echo "-t, --optional also lists packages that are only optional dependencies"
-	exit
+	echo "-ot, --allorphans removes all orphans including packages that are optional dependencies"
+	echo "-lo, --listorphans lists orphans"
+	echo "-lot --listallorphans lists all orphans including packages that are optional dependencies"
 elif [ "$1" == "-o" ] || [ "$1" == "--orphans" ]
 then
 	pacaur -Rns $(pacaur -Qtdq)
-	exit
 elif [ "$1" == "-t" ] || [ "$1" == "--optional" ]
 then
 	comm -23 <(pacaur -Qqtt | sort) <(echo $ignorepkg | tr ' ' '\n' | cat <(pacaur -Sqg $ignoregrp) - | sort -u)
-	exit
+elif [ "$1" == "-ot" ] || [ "$1" == "--allorphans" ]
+then
+	pacaur -Rns $(pacaur -Qttdq)
+elif [ "$1" == "-lo" ] || [ "$1" == "--listorphans" ]
+then 
+	pacaur -Qtdq
+elif [ "$1" == "-lot" ] || [ "$1" == "--listallorphans" ]
+then 
+	pacaur -Qttdq
+else
+	comm -23 <(pacaur -Qqt | sort) <(echo $ignorepkg | tr ' ' '\n' | cat <(pacaur -Sqg $ignoregrp) - | sort -u)
 fi
-
-comm -23 <(pacaur -Qqt | sort) <(echo $ignorepkg | tr ' ' '\n' | cat <(pacaur -Sqg $ignoregrp) - | sort -u)
