7e9539ab9e597d1d48188ed037aebcc5b7dd9f85
rooxo <rooxoxoxoxo@gmail.com>
2017-07-28 15:38:27 +0200

    fixed unisonsync

--- utils/unisonsync.sh
+++ utils/unisonsync.sh
@@ -4,4 +4,4 @@ BACKUPDIR="/mnt/"
 unison ~/Documents/Studium $BACKUPDIR/Documents/Studium -auto
 unison ~/Documents/ebooks $BACKUPDIR/Documents/ebooks -auto
 unison ~/Pictures $BACKUPDIR/Pictures -auto
-unison ~/Documents/feedlist.opml -auto
+unison ~/Documents/feedlist.opml $BACKUPDIR/Documents/feedlist.opml -auto
