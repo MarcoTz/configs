5d2f49dcde0a021141d076f5a6569c6453315128
rooxo <rooxoxoxoxo@gmail.com>
2017-07-31 11:55:16 +0200

    updated nodepend from yoga branch

--- utils/nodepend.sh
+++ utils/nodepend.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
+ignoregrp="base base-devel"
+ignorepkg=""
+
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]
 then
-	echo "lists all packages no other packages depend on(pacaur only)"
+	echo "lists all packages no other packages depend on"
 	echo ""	
 	echo "Options:"
 	echo "-o, --orphans removes orphans(requires root permissions)"	
-	exit
+	echo "-t, --optional also lists packages that are only optional dependencies"
+	echo "-ot, --allorphans removes all orphans including packages that are optional dependencies"
+	echo "-lo, --listorphans lists orphans"
+	echo "-lot --listallorphans lists all orphans including packages that are optional dependencies"
 elif [ "$1" == "-o" ] || [ "$1" == "--orphans" ]
 then
 	pacaur -Rns $(pacaur -Qtdq)
-	exit
+elif [ "$1" == "-t" ] || [ "$1" == "--optional" ]
+then
+	comm -23 <(pacaur -Qqtt | sort) <(echo $ignorepkg | tr ' ' '\n' | cat <(pacaur -Sqg $ignoregrp) - | sort -u)
+elif [ "$1" == "-ot" ] || [ "$1" == "--allorphans" ]
+then
+	pacaur -Rns $(pacaur -Qttdq)
+elif [ "$1" == "-lo" ] || [ "$1" == "--listorphans" ]
+then 
+	pacaur -Qtdq
+elif [ "$1" == "-lot" ] || [ "$1" == "--listallorphans" ]
+then 
+	pacaur -Qttdq
+else
+	comm -23 <(pacaur -Qqt | sort) <(echo $ignorepkg | tr ' ' '\n' | cat <(pacaur -Sqg $ignoregrp) - | sort -u)
 fi
-
-ignoregrp="base base-devel"
-ignorepkg=""
-comm -23 <(pacaur -Qqt | sort) <(echo $ignorepkg | tr ' ' '\n' | cat <(pacaur -Sqg $ignoregrp) - | sort -u)
