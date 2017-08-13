07d9ec1c228c36896966954e4299d31d1314e1b8
rooxo <rooxoxoxoxo@gmail.com>
2017-08-13 14:08:15 +0200

    updated unisonsync

--- utils/unisonsync.sh
+++ utils/unisonsync.sh
@@ -2,6 +2,8 @@
 BACKUPDIR="/mnt/"
 
 unison ~/Documents/Studium $BACKUPDIR/Documents/Studium -auto
+unison ~/Documents/Cheruskia $BACKUPDIR/Documents/Cheruskia -auto
+unison ~/Documents/Konto $BAKCKUPDIR/Documents/Konto -auto
 unison ~/Documents/ebooks $BACKUPDIR/Documents/ebooks -auto
 unison ~/Pictures $BACKUPDIR/Pictures -auto
 unison ~/Documents/feedlist.opml $BACKUPDIR/Documents/feedlist.opml -auto
