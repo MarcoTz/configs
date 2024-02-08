4da60508e3ce4734435c35da58eb70fb351e607c
MarcoTz <marco.tzschentke@gmail.com>
2024-02-08 17:29:42 +0100

    fixed coc config

--- nvim/create_symlinks.sh
+++ nvim/create_symlinks.sh
@@ -4,4 +4,4 @@ rm ~/.config/nvim/init.vim
 ln -s $PWD/init.vim ~/.config/nvim/init.vim
 
 rm ~/.config/nvim/coc-settings.json
-ln -s $PWD/coc-settings.vim ~/.config/nvim/coc-settings.json
+ln -s $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
