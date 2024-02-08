9606e83344b301152b832d963a960ed46f4b82df
MarcoTz <marco.tzschentke@gmail.com>
2024-02-08 12:33:40 +0100

    moved ASUS to old

---
+++ old/setupScripts2/installConfs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
+cd $DIR
+
+#removes old config files if -f or --force is present
+if [ "$1" == "-f" ] || [ "$1" == "--force" ]
+then 
+	rm ~/.bashrc
+	rm ~/.vimrc
+fi
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]
+then
+	echo "installs configs in config directory of this repo"
+	echo "options:"
+	echo "-h, --help print this help and exit"
+	echo "-f, --force remove the configs first if they exist"
+fi
+
+ln -s $DIR/configs/bashrc ~/.bashrc
+ln -s $DIR/configs/vimrc ~/.vimrc
+
